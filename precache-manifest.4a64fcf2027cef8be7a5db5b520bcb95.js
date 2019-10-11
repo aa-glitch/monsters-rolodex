64a6e362dd857bee16ebf7150d7719bc7280a2ff
Anshul Anand <ansh.anand@gmail.com>
2019-10-10 21:02:14 -0400

    Updates

---
+++ precache-manifest.4a64fcf2027cef8be7a5db5b520bcb95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61eac49a65eb639f3706870f8a977f0c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dd3424568e77b2abb1d2",
+    "url": "/monsters-rolodex/static/css/main.ef34a58a.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monsters-rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "dd3424568e77b2abb1d2",
+    "url": "/monsters-rolodex/static/js/main.425a5db1.chunk.js"
+  },
+  {
+    "revision": "d71975295fe45a6cbab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.4686dfee.js"
+  }
+]);
